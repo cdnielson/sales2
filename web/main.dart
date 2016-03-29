712b0418d1ef2d53dd3046ed8bd87eff9f0a655b
cdnielson <cdnielson@folkprophet.com>
2016-03-29 08:03:43 -0600

    removed polymer from pubspec...broke it

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
 
-import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
-import 'package:polymer/polymer.dart';
+//import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
+//import 'package:polymer/polymer.dart';
 
 import 'package:angular2/angular2.dart';
 import 'package:angular2/bootstrap.dart';
