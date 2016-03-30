3b8e7330efee7e198cd2530e8b7b5f999b6e0509
cdnielson <cdnielson@folkprophet.com>
2016-03-30 07:50:44 -0600

    Copied and replaced files to get the non-polymer stuff working in an Angular 2 template

--- README.md
+++
@@ -1,4 +0,0 @@
-Tic-Tac-Toe
-======
-
-An implementation of the classic game built with Dart, Angular 2, and Polymer. This version requires two human players.
--- web/main.dart
+++ web/main.dart
@@ -1,27 +1,10 @@
-import 'dart:html';
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
 
-//import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
-//import 'package:polymer/polymer.dart';
+import 'package:angular2/platform/browser.dart';
 
-import 'package:angular2/angular2.dart';
-import 'package:angular2/bootstrap.dart';
-
-import 'package:sales2/views/main_app/main_app.dart';
-import 'package:sales2/services/logger.dart';
-
-import 'package:logging/logging.dart';
-
-const String APP_NAME = "Sales";
-
-final AppMode appMode = window.location.host.contains('localhost') ? AppMode.Develop : AppMode.Production;
-
-final OpaqueToken AppNameToken = new OpaqueToken("AppNameToken");
+import 'package:Sales2_2/app_component.dart';
 
 main() {
-  bootstrap(MainApp, [
-    provide(AppNameToken, useValue: APP_NAME),
-    provide(AppMode, useValue: appMode),
-    provide(Logger, useFactory: (String name, AppMode mode) =>
-        initLog(name, mode), deps: [AppNameToken, AppMode])
-  ]);
+  bootstrap(AppComponent);
 }
